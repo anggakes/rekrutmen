d9eaf26b4d2da0141ffa2a7a402b02711b6f4115
unknown <anggakesuma@gmail.com>
2015-03-08 21:20:08 +0700

    tinggal revisi

--- application/views/karir/home.php
+++ application/views/karir/home.php
@@ -1,8 +1,7 @@
             <div class="col-sm-8">
                 <h2>Bergabunglah Bersama Kami</h2>
-                <p>Introduce the visitor to the business using clear, informative text. Use well-targeted keywords within your sentences to make sure search engines can find the business.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et molestiae similique eligendi reiciendis sunt distinctio odit? Quia, neque, ipsa, adipisci quisquam ullam deserunt accusantium illo iste exercitationem nemo voluptates asperiores.</p>
-                <p>
-                    <a class="btn btn-default btn-lg" href="#">Call to Action &raquo;</a>
-                </p>
+                <p>Sebagai perusahaan yang terkemuka di Indonesia dan berkembang di dunia, Telkom Group yang bergerak di bidang Telekomunikasi, Informasi, Media, Edutainment dan Servis dengan jumlah karyawan lebih dari 25000 orang di seluruh dunia mengajak anda, putra dan putri terbaik, untuk menjelajahi kesempatan karir bersama kami.
+</p><p>
+Didukung dengan berbagai program pengembangan dan perencanaan karir, kami berkomitmen untuk mengeksplorasi dan mengembangkan kemampuan setiap individu, karena kami memahami, karyawan adalah asset kami yang paling berharga. Rencanakan masa depan anda sekarang dan bergabunglah bersama keluarga Telkom Group untuk menciptakan masa depan kehidupan digital..</p>
+               
             </div>
